b025f8e20dd8ddfbd84d7c3cf5d6058a9f8600c3
bytestreme <44555427+bytestreme@users.noreply.github.com>
2021-10-14 00:50:47 +0600

    dart format

--- lib/profile/bloc/profile_state.dart
+++ lib/profile/bloc/profile_state.dart
@@ -1,7 +1,7 @@
 part of 'profile_bloc.dart';
 
 @freezed
-class ProfileState with _$ProfileState{
+class ProfileState with _$ProfileState {
   const factory ProfileState.initial() = _initial;
 
   const factory ProfileState.loaded(Profile profile) = _loaded;
