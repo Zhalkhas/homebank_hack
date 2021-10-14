2be5da14c019182471f488566d7e7d80ac221016
zhalgas <zhalgas.khassenov@buhta.com>
2021-10-14 12:09:46 +0600

    added home after loading page

---
+++ lib/app/view/theme.dart
@@ -0,0 +1,306 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: const Color(0xfff3253c),
+  primaryColorBrightness: Brightness.dark,
+  primaryColorLight: const Color(0xfffccfd4),
+  primaryColorDark: const Color(0xff910817),
+  canvasColor: const Color(0xfffafafa),
+  scaffoldBackgroundColor: const Color(0xfffafafa),
+  bottomAppBarColor: const Color(0xffffffff),
+  cardColor: const Color(0xffffffff),
+  dividerColor: const Color(0x1f000000),
+  highlightColor: const Color(0x66bcbcbc),
+  splashColor: const Color(0x66c8c8c8),
+  selectedRowColor: const Color(0xfff5f5f5),
+  unselectedWidgetColor: const Color(0x8a000000),
+  disabledColor: const Color(0x61000000),
+  toggleableActiveColor: const Color(0xffc10b1f),
+  secondaryHeaderColor: const Color(0xfffee7e9),
+  backgroundColor: const Color(0xfffa9ea9),
+  dialogBackgroundColor: const Color(0xffffffff),
+  indicatorColor: const Color(0xfff20d27),
+  hintColor: const Color(0x8a000000),
+  errorColor: const Color(0xffd32f2f),
+  buttonTheme: const ButtonThemeData(
+    padding: EdgeInsets.only(left: 16, right: 16),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(2)),
+    ),
+    buttonColor: Color(0xffe0e0e0),
+    disabledColor: Color(0x61000000),
+    highlightColor: Color(0x29000000),
+    splashColor: Color(0x1f000000),
+    focusColor: Color(0x1f000000),
+    hoverColor: Color(0x0a000000),
+    colorScheme: ColorScheme(
+      primary: Color(0xfff3253c),
+      primaryVariant: Color(0xff910817),
+      secondary: Color(0xfff20d27),
+      secondaryVariant: Color(0xff910817),
+      surface: Color(0xffffffff),
+      background: Color(0xfffa9ea9),
+      error: Color(0xffd32f2f),
+      onPrimary: Color(0xffffffff),
+      onSecondary: Color(0xffffffff),
+      onSurface: Color(0xff000000),
+      onBackground: Color(0xffffffff),
+      onError: Color(0xffffffff),
+      brightness: Brightness.light,
+    ),
+  ),
+  textTheme: const TextTheme(
+    headline1: TextStyle(
+      color: Color(0x8a000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    headline2: TextStyle(
+      color: Color(0x8a000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    headline3: TextStyle(
+      color: Color(0x8a000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    headline4: TextStyle(
+      color: Color(0x8a000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    headline5: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    headline6: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    subtitle1: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    bodyText1: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    bodyText2: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    caption: TextStyle(
+      color: Color(0x8a000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    button: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    subtitle2: TextStyle(
+      color: Color(0xff000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    overline: TextStyle(
+      color: Color(0xff000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+  ),
+  primaryTextTheme: const TextTheme(
+    headline1: TextStyle(
+      color: Color(0xb3ffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    headline2: TextStyle(
+      color: Color(0xb3ffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    headline3: TextStyle(
+      color: Color(0xb3ffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    headline4: TextStyle(
+      color: Color(0xb3ffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    headline5: TextStyle(
+      color: Color(0xffffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    headline6: TextStyle(
+      color: Color(0xffffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    subtitle1: TextStyle(
+      color: Color(0xffffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    bodyText1: TextStyle(
+      color: Color(0xffffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    bodyText2: TextStyle(
+      color: Color(0xffffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    caption: TextStyle(
+      color: Color(0xb3ffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    button: TextStyle(
+      color: Color(0xffffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    subtitle2: TextStyle(
+      color: Color(0xffffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    overline: TextStyle(
+      color: Color(0xffffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+  ),
+  inputDecorationTheme: const InputDecorationTheme(
+    labelStyle: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    helperStyle: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    hintStyle: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    errorStyle: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    contentPadding: EdgeInsets.only(top: 12, bottom: 12),
+    prefixStyle: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    suffixStyle: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    counterStyle: TextStyle(
+      color: Color(0xdd000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    fillColor: Color(0x00000000),
+    errorBorder: UnderlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(4)),
+    ),
+    focusedBorder: UnderlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(4)),
+    ),
+    focusedErrorBorder: UnderlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(4)),
+    ),
+    disabledBorder: UnderlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(4)),
+    ),
+    enabledBorder: UnderlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(4)),
+    ),
+    border: UnderlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(4)),
+    ),
+  ),
+  iconTheme: const IconThemeData(
+    color: Color(0xdd000000),
+    opacity: 1,
+    size: 24,
+  ),
+  primaryIconTheme: const IconThemeData(
+    color: Color(0xffffffff),
+    opacity: 1,
+    size: 24,
+  ),
+  sliderTheme: const SliderThemeData(
+    valueIndicatorTextStyle: TextStyle(
+      color: Color(0xffffffff),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+  ),
+  tabBarTheme: const TabBarTheme(
+    indicatorSize: TabBarIndicatorSize.tab,
+    labelColor: Color(0xffffffff),
+    unselectedLabelColor: Color(0xb2ffffff),
+  ),
+  chipTheme: const ChipThemeData(
+    backgroundColor: Color(0x1f000000),
+    brightness: Brightness.light,
+    deleteIconColor: Color(0xde000000),
+    disabledColor: Color(0x0c000000),
+    labelPadding: EdgeInsets.only(left: 8, right: 8),
+    labelStyle: TextStyle(
+      color: Color(0xde000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    padding: EdgeInsets.only(top: 4, bottom: 4, left: 4, right: 4),
+    secondaryLabelStyle: TextStyle(
+      color: Color(0x3d000000),
+      fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+    ),
+    secondarySelectedColor: Color(0x3df3253c),
+    selectedColor: Color(0x3d000000),
+    shape: StadiumBorder(),
+  ),
+  dialogTheme: const DialogTheme(shape: RoundedRectangleBorder()),
+  textSelectionTheme: const TextSelectionThemeData(
+    cursorColor: Color(0xff4285f4),
+    selectionColor: Color(0xfffa9ea9),
+    selectionHandleColor: Color(0xfff76e7d),
+  ),
+  colorScheme: ColorScheme.fromSwatch(
+      primarySwatch: const MaterialColor(4294124860, {
+    50: Color(0xfffee7e9),
+    100: Color(0xfffccfd4),
+    200: Color(0xfffa9ea9),
+    300: Color(0xfff76e7d),
+    400: Color(0xfff43e52),
+    500: Color(0xfff20d27),
+    600: Color(0xffc10b1f),
+    700: Color(0xff910817),
+    800: Color(0xff610510),
+    900: Color(0xff300308)
+  })).copyWith(secondary: const Color(0xfff20d27)),
+);
